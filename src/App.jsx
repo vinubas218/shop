552ff190e87b3153ae1588b6fc7ea3e20f0ded67
vinubas218 <vinubas960221106107@gmail.com>
2025-01-21 20:22:42 +0530

    path changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,30 @@
 import React from 'react'
-
-import Offer from './Offer';
-import NewArrivals from './NewArrivals';
+import {BrowserRouter} from 'react-router-dom'
+import {Routes, Route} from 'react-router-dom'
 import Kids from './Kids';
 import Men from './Men';
 import Women from './Women'
 import Home from './Home'
-
-import {BrowserRouter} from 'react-router-dom'
-import {Routes, Route} from 'react-router-dom'
 import Signin from './Signin ';
 import Register from './Register'
 import CartUI from './CartUI';
-import { useState } from 'react';
+
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          {/* <Route path='/Head'>{<Head/>}</Route> */}
-          <Route path='/Home' element = {<Home/>}/>
-          <Route path='/Signin' element = {<Signin/>}/>
-          <Route path='/Register' element = {<Register/>}/>
-          <Route path='/Men' element = {<Men/>}/>
-          <Route path='/Women' element = {<Women/>}/>
-          <Route path='/Kids' element = {<Kids/>}/>
-          <Route path='/CartUI' element = {<CartUI/>}/>
+          <Route path='/' element= {<Home/>}/>
+          <Route path='/home' element = {<Home/>}/>
+          <Route path='/signin' element = {<Signin/>}/>
+          <Route path='/register' element = {<Register/>}/>
+          <Route path='/men' element = {<Men/>}/>
+          <Route path='/women' element = {<Women/>}/>
+          <Route path='/kids' element = {<Kids/>}/>
+          <Route path='/cartUI' element = {<CartUI/>}/>
         </Routes>
       </BrowserRouter>
-
-
-      
-
-      
-
-
-      {/* 
-      <Men></Men>
-      <Women></Women>
-
-      <Kids></Kids>
-
-
-
-
-
-
-      <NewArrivals></NewArrivals>
-      <Offer></Offer> */}
-
-
-
-
     </div>
 
   )
